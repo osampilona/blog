68e01f4042cf382c630e598b1159a8734a91fe3f
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2023-09-10 10:20:52 +0200

    Home and Navbar components and click (#3)

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+  const handleClick = () => {};
+
+  return (
+    <div>
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+};
+
+export default Home;
