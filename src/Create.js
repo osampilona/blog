9c650100b6881afcb5c23bdc9763050fa8eea1e3
Martina Brzica <78907989+osampilona@users.noreply.github.com>
2023-09-16 08:36:37 +0200

    Form and submitting the form (#7)

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Mario");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+  };
+
   return (
     <div className="create">
       <h2>Add new blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
   );
 };
